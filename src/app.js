d83b510b2a1278e478849df05bb6dbb680c5a62f
Sebastian Golian <sebastiangolian@gmail.com>
2019-01-27 21:58:03 +0100

    add jquery and grid

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/dropdown';
+import 'jquery/dist/jquery';
 import './app.scss';
+
+console.log($.fn.tooltip.Constructor.VERSION);
+
+// document.getElementById("version").innerHTML = ;
+// document.getElementById("version").innerHTML = "test";
