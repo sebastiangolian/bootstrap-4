77b5300e73c03b59ed8505e41f657410d958510f
Sebastian Golian <sebastiangolian@gmail.com>
2019-01-28 21:56:53 +0100

    add grid and sidebar page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/dropdown';
-import 'jquery/dist/jquery';
 import './app.scss';
-
-console.log($.fn.tooltip.Constructor.VERSION);
-
-// document.getElementById("version").innerHTML = ;
-// document.getElementById("version").innerHTML = "test";
