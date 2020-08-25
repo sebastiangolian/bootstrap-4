8192b188eb3bf204431b22f9fce3e73c6121952f
Sebastian Golian <sebastiangolian@gmail.com>
2020-08-25 14:39:01 +0200

    update project

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   watchOptions: {
-    aggregateTimeout: 1000,
-    poll: 5000,
+    aggregateTimeout: 200,
+    poll: 1000,
     ignored: ['node_modules']
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
+    contentBase: path.join(__dirname, 'src'),
+    watchContentBase: true
   },
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        //path.resolve(__dirname, 'src'),
+        {
+          from: 'src/*.html',
+          to: 'dist/',
+          context: 'app/',
+        },
+      ],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
   module: {
     rules: [
       {
@@ -35,7 +50,7 @@ module.exports = {
             }
           },
         ]
-      }
+      },
     ]
   }
 };
